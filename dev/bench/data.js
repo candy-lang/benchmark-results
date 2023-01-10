df15497fe0b622b778e94403dcc774fb546e414e
github-action-benchmark <github@users.noreply.github.com>
2023-01-10 10:06:52 +0000

    add Compiler (cargo) benchmark result for 8cd5d7eb8007811b6c8dbfc310beeb2e13cab3e8

---
+++ dev/bench/data.js
@@ -0,0 +1,40 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1673345212024,
+  "repoUrl": "https://github.com/candy-lang/candy",
+  "entries": {
+    "Compiler": [
+      {
+        "commit": {
+          "author": {
+            "name": "candy-lang",
+            "username": "candy-lang"
+          },
+          "committer": {
+            "name": "candy-lang",
+            "username": "candy-lang"
+          },
+          "id": "8cd5d7eb8007811b6c8dbfc310beeb2e13cab3e8",
+          "message": "Benchmark CI",
+          "timestamp": "2023-01-07T02:55:45Z",
+          "url": "https://github.com/candy-lang/candy/pull/263/commits/8cd5d7eb8007811b6c8dbfc310beeb2e13cab3e8"
+        },
+        "date": 1673345211574,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "VM Runtime/hello_world",
+            "value": 16622798,
+            "range": "± 1763747",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "VM Runtime/fibonacci/15",
+            "value": 564313335,
+            "range": "± 11936706",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
