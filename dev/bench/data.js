4560eaecc30ef07efab6f0bb08350318e841516d
github-action-benchmark <github@users.noreply.github.com>
2023-01-10 19:37:08 +0000

    add Compiler (cargo) benchmark result for b9938236ea2d1b104b0b7969a434b8bdb2bf3170

---
+++ dev/bench/data.js
@@ -0,0 +1,40 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1673379428944,
+  "repoUrl": "https://github.com/candy-lang/candy",
+  "entries": {
+    "Compiler": [
+      {
+        "commit": {
+          "author": {
+            "name": "candy-lang",
+            "username": "candy-lang"
+          },
+          "committer": {
+            "name": "candy-lang",
+            "username": "candy-lang"
+          },
+          "id": "b9938236ea2d1b104b0b7969a434b8bdb2bf3170",
+          "message": "Text interpolation",
+          "timestamp": "2023-01-07T02:55:45Z",
+          "url": "https://github.com/candy-lang/candy/pull/258/commits/b9938236ea2d1b104b0b7969a434b8bdb2bf3170"
+        },
+        "date": 1673379428573,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "VM Runtime/hello_world",
+            "value": 11237507,
+            "range": "± 263249",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "VM Runtime/fibonacci/15",
+            "value": 435609237,
+            "range": "± 3830899",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
