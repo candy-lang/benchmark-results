10ed8d733bbcc7d62cc271fb40ee7e4904cd670a
github-action-benchmark <github@users.noreply.github.com>
2023-01-10 12:50:48 +0000

    add Compiler (cargo) benchmark result for 1d8a5b4ebc13182fe6ecb801d8a7012f42cefff7

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1673345212024,
+  "lastUpdate": 1673355048651,
   "repoUrl": "https://github.com/candy-lang/candy",
   "entries": {
     "Compiler": [
@@ -31,6 +31,38 @@ window.BENCHMARK_DATA = {
             "name": "VM Runtime/fibonacci/15",
             "value": 564313335,
             "range": "± 11936706",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "candy-lang",
+            "username": "candy-lang"
+          },
+          "committer": {
+            "name": "candy-lang",
+            "username": "candy-lang"
+          },
+          "id": "1d8a5b4ebc13182fe6ecb801d8a7012f42cefff7",
+          "message": "Don't clone MIR for optimizations",
+          "timestamp": "2023-01-07T02:55:45Z",
+          "url": "https://github.com/candy-lang/candy/pull/264/commits/1d8a5b4ebc13182fe6ecb801d8a7012f42cefff7"
+        },
+        "date": 1673355048178,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "VM Runtime/hello_world",
+            "value": 11241997,
+            "range": "± 183377",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "VM Runtime/fibonacci/15",
+            "value": 444014133,
+            "range": "± 3643338",
             "unit": "ns/iter"
           }
         ]
